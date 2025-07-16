f7b104a0216912a015e919e8d9e8735e162508ec
Julio-C-Oliveira <julio3.oliveir@gmail.com>
2025-07-16 09:24:57 -0300

    feat(front): Adiciona o front na branch main.

---
+++ front-end/nexus-front/src/App.tsx
@@ -0,0 +1,5 @@
+import PagesRoutes from "./Components/PagesRoutes"
+
+export default function App() {
+  return <PagesRoutes/>
+}
---
+++ front-end/nexus-front/src/Components/PagesRoutes.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import { DefaultLayout } from "./DefaultLayout";
+import { GlobalContext } from "./GlobalContext";
+
+import { LandingPage } from "./Pages/LandingPage/LandingPage";
+import { Login } from "./Pages/Login/Login";
+import { Home } from "./Pages/Home/Home";
+import { Results } from "./Pages/Results/Results";
+import { Project } from "./Pages/Project/Project";
+
+export default function PagesRoutes(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GlobalContext/>}>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/login" element={<Login/>}/>
+
+          <Route path="/inicio" element={<DefaultLayout/>}>
+            <Route path="/inicio" element={<Home/>}/>
+            <Route path="/inicio/resultados" element={<Results/>}/>
+            <Route path="/inicio/projeto" element={<Project/>}/>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
