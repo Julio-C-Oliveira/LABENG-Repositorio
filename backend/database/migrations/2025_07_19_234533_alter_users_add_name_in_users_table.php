88a0c8c074fa5573a1bd300b83a476a6e6fc803f
Júlio C Oliveira <julio3.oliveir@gmail.com>
2025-08-12 10:23:44 -0300

    fix(migration): Corrige erro de migration +
    O arquivo 2025_07_19_234533_alter_users_add_name_in_users_table.php
    estava gerando erro, devido a linha
    $table->string("name")->after("id").
    Substitui a string "id" por "user_id", que é a coluna correta.

--- backend/database/migrations/2025_07_19_234533_alter_users_add_name_in_users_table.php
+++ backend/database/migrations/2025_07_19_234533_alter_users_add_name_in_users_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::table("users", function (Blueprint $table) {
-            $table->string("name")->after("id");
+            $table->string("name")->after("user_id");
         });
     }
 
